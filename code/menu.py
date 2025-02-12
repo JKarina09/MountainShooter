0119854c7bd4b2b27716464aac2daf037fe5ff57
karina.neves <karina_tlw@yahoo.com.br>
2025-02-12 08:41:29 -0300

    adding classes generated from StarUML

--- code/menu.py
+++ code/menu.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from lib2to3.pytree import convert
+from msilib.schema import Font
+
 import pygame.image
 import pygame
+from pygame import Surface, Rect
+
+from code.Const import WIN_WIDTH
+
+
 class Menu:
     def __init__(self, window):
         self.window = window
@@ -10,7 +18,22 @@ class Menu:
 
 
     def run(self):
-        self.window.blit(source=self.surf, dest=self.rect)
-        pygame.display.flip()
-        pass
- 
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(text_size=50, text='Mountain',text_color=(255, 125, 0), text_center_pos=((WIN_WIDTH/2),70))
+            pygame.display.flip()
+
+           #check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple) -> None:
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
